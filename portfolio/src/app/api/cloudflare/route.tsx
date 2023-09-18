ff6b590a28c0d52df802d4f8104405757af0c2f2
Anton Hagsér <anton.hagser@epsidel.se>
2023-09-18 19:55:52 +0200

    fix: remove logging

--- portfolio/src/app/api/cloudflare/route.tsx
+++ portfolio/src/app/api/cloudflare/route.tsx
@@ -1,8 +1,6 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
-    console.log("GET /api/cloudflare: " + process.env.CLOUDFLARE_TURNSTILE_SITE_KEY);
-
     return NextResponse.json({
         cloudflareSiteKey: process.env.CLOUDFLARE_TURNSTILE_SITE_KEY,
     });
